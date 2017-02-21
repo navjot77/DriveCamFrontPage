d9ad4c6fe757d3b29a363b6c4d0143995dbbf29a
navjot77 <mail.navjot77@gmail.com>
2017-02-20 23:03:24 -0800

    Added js events for overlay grids

---
+++ engineapp/static/blog.js
@@ -0,0 +1,11 @@
+/**
+ * Created by navjotsingh on 2/19/17.
+ */
+
+ $(document).ready(function () {
+
+$("#grid1").mouseenter(function(){ document.getElementById("overlay_grid").style.height = "400px";});
+$("#grid1").mouseleave(function(){ document.getElementById("overlay_grid").style.height = "0px";});
+
+
+ });
